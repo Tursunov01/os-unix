f55b7506a8be44f969737ae8c7e1890d4ce6a818
Jasur Tursunov <tursunov.jasur@outlook.com>
2020-11-30 20:32:40 +0500

    task1 and task2 have been changed

--- lab4/task1.c
+++ lab4/task1.c
@@ -4,24 +4,38 @@
 
 int main()
 {
-	int child = fork();
-	if ( child == -1 )
+	int child1 = fork();
+	if ( child1 == -1 )
 	{
 		perror("couldn't fork.");
 		exit(1);
 	}
-	if ( child == 0 )
+	if ( child1 == 0 )
 	{
 		//потомственный код
 		sleep(1);
-		printf( "Child: pid=%d;	group=%d;	parent=%d\n", getpid(), getpgrp(), getppid() );
+		printf( "Child1: pid=%d;	group=%d;	parent=%d\n", getpid(), getpgrp(), getppid() );
+
+		return 0;
+	}
+	int child2 = fork();
+	if ( child2 == -1 )
+	{
+		perror("couldn't fork.");
+		exit(1);
+	}
+	if ( child2 == 0 )
+	{
+		//потомственный код
+		sleep(1);
+		printf( "Child2: pid=%d;	group=%d;	parent=%d\n", getpid(), getpgrp(), getppid() );
 
 		return 0;
 	}
 	else
 	{
 		//родительский код
-        printf( "Parent: pid=%d;	group=%d;	child=%d\n", getpid(), getpgrp(), child );
+        printf( "Parent: pid=%d;	group=%d;	child1=%d;   child2=%d\n", getpid(), getpgrp(), child1, child2 );
 		return 0;
 	}
 }
